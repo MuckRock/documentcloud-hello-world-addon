3740778d627864c1900d2c37eac38c7c11a07c23
Mitchell Kotler <mitch@mitchellkotler.net>
2022-02-08 10:24:31 -0500

    accept a query

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import documentcloud
 
 
 def load_params():
-    """Load the parameters passed in to the GitHub Action"""
+    """Load the parameters passed in to the GitHub Action."""
     params = json.loads(sys.argv[1])
     # token is a JWT to use to authenticate against the DocumentCloud API
     token = params.pop("token")
@@ -23,33 +23,39 @@ def load_params():
     base_uri = params.pop("base_uri", None)
     # Documents is a list of document IDs which were selected to run with this
     # plugin activation
-    documents = params.pop("documents")
+    documents = params.pop("documents", None)
+    # Query is the search query selected to run with this plugin activation
+    query = params.pop("query", None)
     # params will contain add on specific data in 'data', and the user and org
     # IDs in 'user_id' and 'org_id'
-    return token, base_uri, documents, params
+    return token, base_uri, documents, query, params
 
 
 def init():
-    """Load the paraneters and initialize the DocumentCloud client"""
-    token, base_uri, params = load_params()
+    """Load the paraneters and initialize the DocumentCloud client."""
+    token, base_uri, documents, query, params = load_params()
     kwargs = {'base_uri': base_uri} if base_uri is not None else {}
     client = documentcloud.DocumentCloud(**kwargs)
     client.session.headers.update({"Authorization": "Bearer {}".format(token)})
     client.session.headers["User-Agent"] += " (DC AddOn)"
-    return client, documents, params
+    return client, documents, query, params
 
 
-def main(client, documents, params):
-    """The main plugin functionality goes here"""
+def main(client, documents, query, params):
+    """The main plugin functionality goes here."""
     # fetch your plugin specific data
     name = params['data'].get("name", "world")
 
     # add a hello note to the first page of each selected document
-    for doc_id in documents:
-        document = client.documents.get(doc_id)
-        document.annotations.create(f"Hello {name}!", 0)
+    if documents:
+        for doc_id in documents:
+            document = client.documents.get(doc_id)
+            document.annotations.create(f"Hello {name}!", 0)
+    elif query:
+        for document in client.documents.search(query):
+            document.annotations.create(f"Hello {name}!", 0)
 
 
 if __name__ == "__main__":
-    client, documents, params = init()
-    main(client, documents, params)
+    args = init()
+    main(*args)
