78b95e6a9969e2c7f39cd5a56252fe3d8a22f07e
Mitchell Kotler <mitch@mitchellkotler.net>
2022-02-08 10:15:57 -0500

    accept arbitrary metadata

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ def load_params():
     """Load the parameters passed in to the GitHub Action"""
     params = json.loads(sys.argv[1])
     # token is a JWT to use to authenticate against the DocumentCloud API
-    token = params["token"]
+    token = params.pop("token")
     # base_uri is the URI to make API calls to - allows the plugin to function
     # in non-production environments
-    base_uri = params.get("base_uri")
+    base_uri = params.pop("base_uri", None)
     # Documents is a list of document IDs which were selected to run with this
     # plugin activation
-    documents = params["documents"]
-    # data is any plugin specific data sent with this request
-    data = params["data"]
-    return token, base_uri, documents, data
+    documents = params.pop("documents")
+    # params will contain add on specific data in 'data', and the user and org
+    # IDs in 'user_id' and 'org_id'
+    return token, base_uri, documents, params
 
 
 def init():
     """Load the paraneters and initialize the DocumentCloud client"""
-    token, base_uri, documents, data = load_params()
+    token, base_uri, params = load_params()
     kwargs = {'base_uri': base_uri} if base_uri is not None else {}
     client = documentcloud.DocumentCloud(**kwargs)
     client.session.headers.update({"Authorization": "Bearer {}".format(token)})
-    return client, documents, data
+    return client, documents, params
 
 
-def main(client, documents, data):
+def main(client, documents, params):
     """The main plugin functionality goes here"""
     # fetch your plugin specific data
-    name = data.get("name", "world")
+    name = params['data'].get("name", "world")
 
     # add a hello note to the first page of each selected document
     for doc_id in documents:
@@ -50,5 +50,5 @@ def main(client, documents, data):
 
 
 if __name__ == "__main__":
-    client, documents, data = init()
-    main(client, documents, data)
+    client, documents, params = init()
+    main(client, documents, params)
